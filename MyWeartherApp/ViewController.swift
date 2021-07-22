12474aba5af79aa6478ffd6dfb8b7ae0a9a45e1f
Nat Askerka <geeliat@gmail.com>
2021-07-22 22:08:43 +0300

    changes

---
+++ MyWeartherApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MyWeartherApp
+//
+//  Created by Natallia Askerka on 22.07.21.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    enum Weather {
+        case one
+        case two
+        case three
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        view.backgroundColor = .blue
+        let one: String
+    }
+
+
+}
+
